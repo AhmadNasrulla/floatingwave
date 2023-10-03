559fc574d8bdd61f726a045307281e531a58f580
Ahmad Nasrullah <119662017+AhmadNasrulla@users.noreply.github.com>
2023-10-03 15:00:31 +0500

    Update Checkout.js

--- frontend/src/core/Checkout.js
+++ frontend/src/core/Checkout.js
@@ -65,68 +65,35 @@ const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
 
   let deliveryAddress = data.address;
 
-  const buy = () => {
+ const buy = () => {
     setData({ loading: true });
-    // send the nonce to your server
-    // nonce = data.instance.requestPaymentMethod()
-    let nonce;
-    let getNonce = data.instance
-      .requestPaymentMethod()
-      .then((data) => {
-        // console.log(data);
-        nonce = data.nonce;
-        // once you have nonce (card type, card number) send nonce as 'paymentMethodNonce'
-        // and also total to be charged
-        // console.log(
-        //     "send nonce and total to process: ",
-        //     nonce,
-        //     getTotal(products)
-        // );
-        const paymentData = {
-          paymentMethodNonce: nonce,
-          amount: getTotal(products),
-        };
-
-        processPayment(userId, token, paymentData)
-          .then((response) => {
-            console.log(response);
-            // empty cart
-            // create order
-
-            const createOrderData = {
-              products: products,
-              transaction_id: response.transaction.id,
-              amount: response.transaction.amount,
-              address: deliveryAddress,
-            };
-
-            createOrder(userId, token, createOrderData)
-              .then((response) => {
-                emptyCart(() => {
-                  setRun(!run); // run useEffect in parent Cart
-                  console.log('payment success and empty cart');
-                  setData({
-                    loading: false,
-                    success: true,
-                  });
-                });
-              })
-              .catch((error) => {
-                console.log(error);
-                setData({ loading: false });
-              });
-          })
-          .catch((error) => {
-            console.log(error);
-            setData({ loading: false });
+  
+    // Process the order with Cash on Delivery
+    const createOrderData = {
+      products: products,
+      amount: getTotal(products),
+      address: deliveryAddress,
+      paymentMethod: 'CashOnDelivery', // Use a string identifier for COD
+    };
+  
+    createOrder(userId, token, createOrderData)
+      .then((response) => {
+        Promise.resolve(emptyCart(() => {
+          setRun(!run); // run useEffect in parent Cart
+          console.log('Order placed successfully (Cash on Delivery)');
+          setData({
+            loading: false,
+            success: true,
           });
+        }))
+        .catch((error) => {
+          console.log(error);
+          setData({ loading: false });
+        });
+        
       })
-      .catch((error) => {
-        // console.log("dropin error: ", error);
-        setData({ ...data, error: error.message });
-      });
+      
   };
-
   const showDropIn = () => (
     <div onBlur={() => setData({ ...data, error: '' })}>
       {data.clientToken !== null && products.length > 0 ? (
